014ce8300adca1820747eb9d1bce6d3d15509542
Vince Melia <vince.melia@vanquisbank.co.uk>
2019-12-04 16:18:31 +0000

    Code from tutorial 04/12/2019.

---
+++ Code/Game.cs
@@ -0,0 +1,46 @@
+﻿namespace Code
+{
+    public class Game
+    {
+        private const int _pinTotal = 10;
+        private readonly int[] _rolls = new int[21];
+        private int _current;
+
+        public void Roll(int pins)
+        {
+            _rolls[_current] += pins;
+            _current++;
+        }
+
+        public int Score()
+        {
+            var total = 0;
+            var firstInFrame = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[firstInFrame] == _pinTotal)  // Strike.
+                {
+                    total += _pinTotal + _rolls[firstInFrame+1] + _rolls[firstInFrame+2];
+                    firstInFrame++;
+                }
+                else if (IsSpare(firstInFrame))
+                {
+                    total += _pinTotal + _rolls[firstInFrame+2];
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    total += _rolls[firstInFrame] + _rolls[firstInFrame+1];
+                    firstInFrame += 2;
+                }
+            }
+
+            return total;
+        }
+
+        private bool IsSpare(int firstInFrame)
+        {
+            return _rolls[firstInFrame] + _rolls[firstInFrame + 1] == _pinTotal;
+        }
+    }
+}
---
+++ Tests/GameTests.cs
@@ -0,0 +1,102 @@
+﻿using Code;
+using Xunit;
+
+namespace Tests
+{
+    public class GameTests
+    {
+        private readonly Game _game;
+
+        public GameTests()
+        {
+            _game = new Game();
+        }
+
+        [Fact]
+        public void GutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.Equal(0, _game.Score());
+        }        
+        
+        [Theory]
+        [InlineData(1, 20)]
+        [InlineData(2, 40)]
+        [InlineData(3, 60)]
+        [InlineData(4, 80)]
+        public void AllOnesToAllFours(int pins, int expected)
+        {
+            RollMany(20, pins);
+
+            Assert.Equal(expected, _game.Score());
+        }
+
+        [Fact]
+        public void OneSpare()
+        {
+            RollSpare();
+            _game.Roll(3);
+            RollMany(17, 0);
+
+            Assert.Equal(16, _game.Score());
+        }       
+        
+        [Fact]
+        public void OneStrike()
+        {
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            RollMany(17, 0);
+
+            Assert.Equal(24, _game.Score());
+        }
+
+        [Fact]
+        public void PerfectGame()
+        {
+            RollMany(12, 10);
+
+            Assert.Equal(300, _game.Score());
+        }
+
+        [Fact]
+        public void SpareInLastFrame()
+        {
+            RollMany(18, 0);
+            RollSpare();
+            _game.Roll(3);
+
+            Assert.Equal(13, _game.Score());
+        }       
+        
+        [Fact]
+        public void StrikeInLastFrame()
+        {
+            RollMany(18, 0);
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+
+            Assert.Equal(17, _game.Score());
+        }
+
+        private void RollMany(int count, int pins)
+        {
+            for(var i = 0; i < count; i++)
+                _game.Roll(pins);
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }        
+        
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+    }
+}
